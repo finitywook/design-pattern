64875774be3b1c699e54da24adcd6e375d7eadf9
유한욱 <ryu@hanwook.me>
2022-09-17 12:14:53 +0900

    Feat: Abstract Factory(추상 팩터리) 추가

---
+++ src/creational/AbstractFactory.java
@@ -0,0 +1,75 @@
+package creational;
+
+public class AbstractFactory {
+
+    public static void main(String[] args) {
+
+        CarType carType = CarType.HYUNDAI;
+
+        CarFactory factory = switch (carType) {
+            case HYUNDAI -> new HyundaiCarFactory();
+            case KIA -> new KiaCarFactory();
+            case SAMSUNG -> new SamsungCarFactory();
+        };
+
+        Car car = factory.create();
+
+        System.out.println("car.getPrice() = " + car.getPrice());
+    }
+
+    interface Car {
+        int getPrice();
+    }
+
+    interface CarFactory {
+        Car create();
+    }
+
+    enum CarType {
+        HYUNDAI,
+        SAMSUNG,
+        KIA
+    }
+
+    static class HyundaiCarFactory implements CarFactory {
+        @Override
+        public Car create() {
+            return new HyundaiCar();
+        }
+    }
+
+    static class SamsungCarFactory implements CarFactory {
+        @Override
+        public Car create() {
+            return new SamsungCar();
+        }
+    }
+
+    static class KiaCarFactory implements CarFactory {
+        @Override
+        public Car create() {
+            return new KiaCar();
+        }
+    }
+
+    static class HyundaiCar implements Car {
+        @Override
+        public int getPrice() {
+            return 20000000;
+        }
+    }
+
+    static class SamsungCar implements Car {
+        @Override
+        public int getPrice() {
+            return 19000000;
+        }
+    }
+
+    static class KiaCar implements Car {
+        @Override
+        public int getPrice() {
+            return 21000000;
+        }
+    }
+}
